be9f5f3fe987f23c88441cc0369c05c63f9d37d7
biancaoliver22 <biancaalmeida1617@gmail.com>
2023-08-18 19:00:03 -0400

    primeiros codigos

---
+++ cnh.js
@@ -0,0 +1,11 @@
+var cnh=prompt("digite a categoria");
+if (cnh=='A' || cnh== 'A')
+alert ("mob");
+else if(cnh== 'AB' || cnh== 'AB' || cnh== 'ab')
+alert ("moto e/ou carro");
+else if (cnh== 'C' || cnh== 'c')
+alert ("caminhao");
+else if (cnh== 'D' || cnh== 'd')
+alert ("carreta");
+else
+alert ("opçao invalida");
---
+++ matematica.js
@@ -0,0 +1,8 @@
+var numero=prompt("digite um numero")
+alert (Math.sqrt(numero));
+if (Math.asin(60)== Math.sqrt(numero)/z)
+alert ("iguais");
+else
+alert ("diferente");
+//operador ternario
+alert ((Math.sin(60)== Math.sqrt(numero)/z)? "iguais": "diferente");
