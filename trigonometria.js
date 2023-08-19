2530ef70da297d111e9f37ef94daa1521cb4cee5
biancaoliver22 <biancaalmeida1617@gmail.com>
2023-08-18 21:12:29 -0400

    typo: z para 2

--- trigonometria.js
+++ trigonometria.js
@@ -2,11 +2,11 @@ var trigonometria=prompt("escolha\n 1.seno\n 2.coseno\n 3.tangente");
 var angulo=prompt("digite o angulo");
 switch (trigonometria){
     case"1":
-    alert(Math.sin((angulo*Math.PI)/180).toFixed(z));break;
+    alert(Math.sin((angulo*Math.PI)/180).toFixed(2));break;
     case"2":
-    alert(Math.cos((angulo*Math.PI)/180).toFixed(z));break;
+    alert(Math.cos((angulo*Math.PI)/180).toFixed(2));break;
     case"3":
-    alert(Math.tan((angulo*Math.PI)/180).toFixed(z));break;
+    alert(Math.tan((angulo*Math.PI)/180).toFixed(2));break;
     default:
         alert("opçao invalida");
 }
